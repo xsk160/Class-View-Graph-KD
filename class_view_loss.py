6e3966e324e8c6eab9a0e62eeee2fb9550108c90
xsk160 <148360264+xsk160@users.noreply.github.com>
2025-02-11 14:29:35 +0800

    Update class_view_loss.py

--- class_view_loss.py
+++ class_view_loss.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn.functional as F
 
-
 def class_loss(logits_mlp, logits_gnn, temperature):
     # Taking the soft label loss function as an example, compute softmax probabilities over the class-view dimension.
     pred_mlp = F.softmax(logits_mlp / temperature, dim=1)
     pred_gnn = F.softmax(logits_gnn / temperature, dim=1)
 
+    log_pred_mlp = torch.log(pred_mlp)
+    
     loss = F.kl_div(
         pred_mlp,
         pred_gnn,
